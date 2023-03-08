168732ddc8ec8bac18008c887a829f20b7ca1114
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-03-07 21:23:56 -0300

    Add files via upload

--- aula/src/aula/Main.java
+++ aula/src/aula/Main.java
@@ -5,18 +5,33 @@ public class Main {
 
 	public static void main(String args[]) {
 		System.out.println("hello world!");
-		System.out.println("syso CTRL+espaço");
 		
 		Scanner sc = new Scanner(System.in);
+		Pessoa p1 = new Pessoa();
 		
 		System.out.println("Digite seu nome");
 		String nome = sc.nextLine();
+		p1.setNome(nome);
 
 		System.out.println("Digite sua idade: ");
-		int idade = sc.nextInt();
+		short idade = sc.nextShort();
+		p1.setIdade(idade);
 		
-		System.out.println("O nome eh "+nome+ " e a sua idade eh "+idade);
+		System.out.println("Nome: "+ p1.getNome()+"\nIdade: "+ p1.getIdade());
 		
+		//System.out.println("Seu nome é "+nome+ " e a sua idade é "+idade);
+		
+		System.out.println("-Fim-");
+	
+		/*
+		 * CTL+D apaga uma linha 
+		 * CTL+Y refaz o que foi desfeito
+		 * CTL+L vai para linha procurada
+		 * CTL+Shift+/ transforma em comentario
+		 * syso CTRL+espaço
+		 * ALT + cima ou baixo
+		 * botao direito, source, generate gets e sets
+		 */
 	}
 
 }
---
+++ aula/src/aula/Pessoa.java
@@ -0,0 +1,20 @@
+package aula;
+
+public class Pessoa {
+	String nome;
+	short idade;
+
+	void setNome(String nome){
+		this.nome = nome;
+	}
+	String getNome() {
+		return this.nome;
+	}
+	public short getIdade() {
+		return idade;
+	}
+	public void setIdade(short idade) {
+		this.idade = idade;
+	}
+	
+}
