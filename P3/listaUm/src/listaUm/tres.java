c48772d201ed614022b42bbe0e792cd60a9e38d8
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-04-01 19:39:55 -0300

    tres

--- P3/listaUm/src/listaUm/tres.java
+++ P3/listaUm/src/listaUm/tres.java
@@ -1,5 +1,28 @@
 package listaUm;
 
-public class tres {
+import java.util.Scanner;
 
+public class tres {
+	
+	public static void main(String args[]) {
+		String nome, sobrenome;
+		
+		Scanner leitor = new Scanner(System.in);
+	
+		System.out.println("Informe seu nome: ");
+		nome = leitor.nextLine();
+		
+		System.out.println("Informe seu sobrenome: ");
+		sobrenome = leitor.nextLine();
+				
+		if(sobrenome.equals("Wayne")) {
+			System.out.println("Acesso liberado, Sr. Wayne");
+		}else if(sobrenome.equals("Kent")) {
+			System.out.println("Saí daí, mané!");
+		}else if(nome.equals("Diana")) {
+			System.out.println("Bem-vinda, Princesa de Themyscara");
+		}else {
+			System.out.println("Cai fora!");
+		}
+	}
 }
