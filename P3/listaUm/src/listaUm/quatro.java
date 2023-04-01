6ece00c08c0e69f83dda1fbd66b7b46874747bfa
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-04-01 19:56:30 -0300

    quatro

--- P3/listaUm/src/listaUm/quatro.java
+++ P3/listaUm/src/listaUm/quatro.java
@@ -1,5 +1,19 @@
 package listaUm;
 
 public class quatro {
-
+	public static void main(String args[]) {
+		int numero;
+		
+		Scanner leitor = new Scanner(System.in);
+	
+		System.out.println("Informe um numero: ");
+		numero = leitor.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.format("O numero é par e sua raiz cubica é: %.2f", Math.pow(numero, 0.333));
+		}else {
+			System.out.format("O numero é impar e sua raiz quadrada é: %.2f", Math.pow(numero, 0.5));
+		}
+	
+	}
 }
