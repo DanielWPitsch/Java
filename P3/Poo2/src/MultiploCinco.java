73d39b5fad7608cdbe345ae08fb7818863513e7e
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-05-09 20:02:20 -0300

    maisum

---
+++ P3/Poo2/src/MultiploCinco.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MultiploCinco {
+	public static void main(String args[]) {
+		
+		for(int i=200; i>=0; i=i-5) {
+			System.out.println(""+i);
+		}
+		System.out.println("-Fim-");
+	}
+}
