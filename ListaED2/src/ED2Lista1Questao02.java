5e4e31218881e2b9493f6f3d92eaa34789772a5e
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-09-08 19:09:34 -0300

    02

--- ListaED2/src/ED2Lista1Questao02.java
+++ ListaED2/src/ED2Lista1Questao02.java
@@ -1,5 +1,5 @@
 /**
-** Função :
+** Função : Soma de elementos de array recursiva
 ** Autor : Daniel Warella Pitsch
 ** Data : 08/09/2023
 ** Observações:
@@ -8,5 +8,24 @@
 
 
 public class ED2Lista1Questao02 {
-
+	
+	public static void main (String args[]) {
+		double[] array1 = {2.2, 333.1, 45.2, 11.2};
+		int tamanho = array1.length;
+		double soma = soma_array(array1, tamanho);
+		
+		System.out.println("A soma do array deu: "+soma);
+		System.out.println("-Fim-");
+	
+	}
+	
+	public static double soma_array(double[] a, int t) {
+		if(t-1 == 0) {
+			return a[0];
+		}else {
+			return a[t-1] + soma_array(a, t-1);
+		}
+		
+	}
+	
 }
---
+++ ListaED2/src/ED2Lista1Questao03.java
@@ -0,0 +1,14 @@
+
+/**
+** Função :
+** Autor : Daniel Warella Pitsch
+** Data : 08/09/2023
+** Observações:
+*/
+
+public class ED2Lista1Questao03 {
+	public static void main (String args[]) {
+		
+		System.out.println("-Fim-");
+	}
+}
