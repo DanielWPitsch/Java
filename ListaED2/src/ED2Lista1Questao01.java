07b402791d8349cfd567c0db1a1a33867645df83
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-09-08 18:46:05 -0300

    01

--- ListaED2/src/ED2Lista1Questao01.java
+++ ListaED2/src/ED2Lista1Questao01.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
-** Função :
+** Função : Soma frações até pela quantidade de vezes que o divisor é dado
 ** Autor : Daniel Warella Pitsch
 ** Data : 08/09/2023
 ** Observações:
@@ -8,7 +10,23 @@
 
 public class ED2Lista1Questao01 {
 	public static void main (String args[]) {
-		System.out.println("hello");
+		
+		System.out.println("Informe até qual casa de divisao você quer somar\nExemplo: "
+				+ "Se você digitar 3, então será a soma de 1 + 1/2 + 1/3");
+		Scanner sc = new Scanner(System.in);
+		double divisor = sc.nextInt();
+		double resultado = soma_fracao(divisor);
+		
+		System.out.println("A soma é: "+ resultado);
+		System.out.println("-Fim-");
+	}
+	
+	public static double soma_fracao(double n) {
+	    if (n == 1) {
+	    	return 1;
+	    }else {
+	    	return 1/n + soma_fracao(n - 1);
+	    }
 	}
 
 }
